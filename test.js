80da80886814ba3fa30dc70e6c70b4c0bf6470eb
Chaim and Miri <edwardstern242@gmail.com>
2019-11-17 18:59:30 +0000

    commit

---
+++ test.js
@@ -0,0 +1,22 @@
+console.log("this is working");
+
+const array1 = ["a", "b", "c", "d"]
+
+function as(){
+    for(i of array1);
+    return i;
+}
+
+console.log(as(array1));
+
+let emptyObject = {};
+ function addEntry(){
+    inputArray = ["inputName", "inputSks18", "inputPsch18", "inputSks19", "inputContNum", "inputContMeth", "inputRatings", "inputComments"];
+    for(i of inputArray){
+        emptyObject.push("name", i);
+        return emptyObject
+    }
+    
+ };
+ console.log(emptyObject);
+
